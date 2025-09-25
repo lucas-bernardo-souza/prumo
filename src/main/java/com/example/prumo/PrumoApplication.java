36ed42f3990eb9549d362633de267ff44b2925b8
lucas-bernardo-souza <lucasbernardodesouza510@gmail.com>
2025-09-24 21:51:26 -0300

    Criacao do projeto prumo

---
+++ src/main/java/com/example/prumo/PrumoApplication.java
@@ -0,0 +1,13 @@
+package com.example.prumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prumo/PrumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
